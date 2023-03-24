7de005d12ade55aa4cdda897fed9f492ad41425b
Jeanne Allen <tebazele@gmail.com>
2023-03-24 13:06:54 -0600

    init commit

---
+++ towerRedo.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'jeanne-allen.us.auth0.com'
+export const clientId = 'sqlbSe3a1IkpDUog43u7KloBLXDFMzBT'
+export const audience = 'https://jeannecoursework.com'
