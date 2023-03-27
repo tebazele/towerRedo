200840d8c721580bfefde68b673daa58fadeb101
Miles <miles.collins@ymail.com>
2023-03-27 12:26:30 -0600

    hopefully...

--- towerRedo/Services/AccountService.cs
+++ towerRedo/Services/AccountService.cs
@@ -28,7 +28,7 @@ public class AccountService
   {
     Account original = GetProfileByEmail(userEmail);
     original.Name = editData.Name.Length > 0 ? editData.Name : original.Name;
-    original.Picture = editData.Picture.Length > 0 ? editData.Picture : original.Picture;
+    original.Picture = editData.Picture ?? original.Picture;
     return _repo.Edit(original);
   }
 }
