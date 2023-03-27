26f3c8ad96ad01ffe896e80fa883f5cc84673400
Miles <miles.collins@ymail.com>
2023-03-27 11:24:51 -0600

    Comments Server Done?

--- towerRedo/Repositories/CommentsRepository.cs
+++ towerRedo/Repositories/CommentsRepository.cs
@@ -2,10 +2,61 @@ namespace towerRedo.Repositories;
 
 public class CommentsRepository
 {
-    private readonly IDbConnection _db;
+  private readonly IDbConnection _db;
 
-    public CommentsRepository(IDbConnection db)
+  public CommentsRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+
+  internal Comment Create(Comment commentData)
+  {
+    string sql = @"
+    INSERT INTO jaComments
+    (creatorId, eventId, body, isAttending)
+    VALUES
+    (@creatorId, @eventId, @body, @isAttending);
+    SELECT LAST_INSERT_ID();
+    ";
+    int id = _db.ExecuteScalar<int>(sql, commentData);
+    commentData.Id = id;
+    return commentData;
+  }
+
+  internal void Delete(int id)
+  {
+    string sql = @"
+    DELETE FROM jaComments WHERE id = @id; 
+    ";
+    _db.Execute(sql, new { id });
+  }
+
+  internal Boolean Edit(Comment commentData)
+  {
+    string sql = @"
+    UPDATE
+    body = @body,
+    isAttending = @isAttending
+    WHERE id = @id
+    ";
+    int rows = _db.Execute(sql, commentData);
+    return rows > 0;
+  }
+
+  internal Comment GetOne(int commentId)
+  {
+    string sql = @"
+    SELECT
+    c.*,
+    a.*
+    FROM jaComments c
+    JOIN accounts a ON c.creatorId = a.id
+    WHERE c.id = @commentId;
+    ";
+    return _db.Query<Comment, Account, Comment>(sql, (c, a) =>
     {
-        _db = db;
-    }
+      c.Creator = a;
+      return c;
+    }, new { commentId }).FirstOrDefault();
+  }
 }
