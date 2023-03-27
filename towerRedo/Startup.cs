5b005629af0d648dd5b60e94255cd59910d08a8a
Jeanne Allen <tebazele@gmail.com>
2023-03-27 12:02:43 -0600

    dotnet restore done

--- towerRedo/Startup.cs
+++ towerRedo/Startup.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
 
 namespace towerRedo;
 
