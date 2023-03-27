9dddf22bf103f84c4697c71ed96179253fc67356
Miles <miles.collins@ymail.com>
2023-03-27 12:17:21 -0600

    Edit Account

--- towerRedo/Controllers/AccountController.cs
+++ towerRedo/Controllers/AccountController.cs
@@ -4,35 +4,48 @@ namespace towerRedo.Controllers;
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
-    private readonly AccountService _accountService;
-    private readonly Auth0Provider _auth0Provider;
+  private readonly AccountService _accountService;
+  private readonly Auth0Provider _auth0Provider;
 
-    public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  {
+    _accountService = accountService;
+    _auth0Provider = auth0Provider;
+  }
+
+  [HttpGet]
+  [Authorize]
+  public async Task<ActionResult<Account>> Get()
+  {
+    try
     {
-        _accountService = accountService;
-        _auth0Provider = auth0Provider;
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_accountService.GetOrCreateProfile(userInfo));
     }
-
-    [HttpGet]
-    [Authorize]
-    public async Task<ActionResult<Account>> Get()
+    catch (Exception e)
     {
-        try
-        {
-            Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-            return Ok(_accountService.GetOrCreateProfile(userInfo));
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
+      return BadRequest(e.Message);
     }
+  }
 
-    // [HttpPut]
-    // [Authorize]
+  [HttpPut]
+  [Authorize]
+  public async Task<ActionResult<Account>> Edit()
+  {
+    try
+    {
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      Account editedAccount = _accountService.Edit(userInfo, userInfo.Email);
+      return Ok(editedAccount);
+    }
+    catch (Exception e)
+    {
+      return BadRequest(e.Message);
+    }
+  }
 
-    // SECTION TICKETS
+  // SECTION TICKETS
 
-    // [HttpGet]
-    // [Authorize]
+  // [HttpGet]
+  // [Authorize]
 }
