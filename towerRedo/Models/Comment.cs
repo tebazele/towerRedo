6da5be0123b5868c6034ae3ee35fc264bac0bf78
Miles <miles.collins@ymail.com>
2023-03-27 11:24:43 -0600

    Comments Server done?

--- towerRedo/Models/Comment.cs
+++ towerRedo/Models/Comment.cs
@@ -2,9 +2,9 @@ namespace towerRedo.Models;
 
 public class Comment : DbItem<int>
 {
-    public string CreatorId { get; set; }
-    public int EventId { get; set; }
-    public string Body { get; set; }
-    public Boolean IsAttending { get; set; }
-    public Account Creator { get; set; }
+  public string CreatorId { get; set; }
+  public int EventId { get; set; }
+  public string Body { get; set; }
+  public Boolean? IsAttending { get; set; }
+  public Account Creator { get; set; }
 }
--- towerRedo/Services/CommentsService.cs
+++ towerRedo/Services/CommentsService.cs
@@ -1,11 +1,57 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace towerRedo.Services;
 
 public class CommentsService
 {
-    
+
+  private readonly CommentsRepository _repo;
+
+  public CommentsService(CommentsRepository repo)
+  {
+    _repo = repo;
+  }
+
+  internal Comment Create(Comment commentData)
+  {
+    Comment comment = _repo.Create(commentData);
+    return comment;
+  }
+
+  internal Comment GetOne(int commentId)
+  {
+    Comment comment = _repo.GetOne(commentId);
+    if (comment == null)
+    {
+      throw new Exception("That comment doesn't exist.");
+    }
+    return comment;
+  }
+
+  internal Comment Edit(Comment commentData, int commentId)
+  {
+    Comment comment = this.GetOne(commentId);
+    if (commentData.CreatorId != comment.CreatorId)
+    {
+      throw new Exception("You do not have permission to edit this comment.");
+    }
+    comment.Body = commentData.Body ?? comment.Body;
+    comment.IsAttending = commentData.IsAttending ?? comment.IsAttending;
+
+    Boolean isEdited = _repo.Edit(comment);
+    if (isEdited == false)
+    {
+      throw new Exception("Edited did not go through repository correctly.");
+    }
+    return comment;
+  }
+
+  internal string Delete(int commentId, string userId)
+  {
+    Comment comment = this.GetOne(commentId);
+    if (comment.CreatorId != userId)
+    {
+      throw new Exception("You do not have permission to delete this comment.");
+    }
+    _repo.Delete(comment.Id);
+    return "Comment has been successfully deleted";
+  }
 }
