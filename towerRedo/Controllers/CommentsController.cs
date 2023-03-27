493ecf4fe45b2d74ad11a08371715a3f3f0f48cb
Miles <miles.collins@ymail.com>
2023-03-26 20:14:53 -0600

    Added comments and todos

--- towerRedo/Controllers/CommentsController.cs
+++ towerRedo/Controllers/CommentsController.cs
@@ -5,4 +5,25 @@ namespace towerRedo.Controllers;
 public class CommentsController : ControllerBase
 {
 
+  private readonly CommentsService _commentsService;
+  private readonly Auth0Provider _a0;
+
+  public CommentsController(CommentsService commentsService, Auth0Provider a0)
+  {
+    _commentsService = commentsService;
+    _a0 = a0;
+  }
+
+  // CREATE
+  [HttpPost]
+  [Authorize]
+
+  // EDIT -- EXTRA
+  [HttpPut("{id}")]
+  [Authorize]
+
+  // DELETE
+  [HttpDelete("{id}")]
+  [Authorize]
+
 }
---
+++ towerRedo/Controllers/ProfilesController.cs
@@ -0,0 +1,19 @@
+namespace towerRedo.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ProfilesController : ControllerBase
+  {
+    private readonly ProfilesService _profilesService;
+
+    public ProfilesController(ProfilesService profilesService)
+    {
+      _profilesService = profilesService;
+    }
+
+    // GET ONE
+    [HttpGet("{id}")]
+
+
+  }
+}
