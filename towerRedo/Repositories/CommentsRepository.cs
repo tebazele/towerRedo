06d0643ec47054fc88032286c71a7ce33cd7dbe6
Jeanne Allen <tebazele@gmail.com>
2023-03-28 15:30:52 -0600

    sql statement for getting comments after tkt creat

--- towerRedo/Repositories/CommentsRepository.cs
+++ towerRedo/Repositories/CommentsRepository.cs
@@ -2,53 +2,53 @@ namespace towerRedo.Repositories;
 
 public class CommentsRepository
 {
-  private readonly IDbConnection _db;
+    private readonly IDbConnection _db;
 
-  public CommentsRepository(IDbConnection db)
-  {
-    _db = db;
-  }
+    public CommentsRepository(IDbConnection db)
+    {
+        _db = db;
+    }
 
-  // CREATE
-  internal Comment Create(Comment commentData)
-  {
-    string sql = @"
+    // CREATE
+    internal Comment Create(Comment commentData)
+    {
+        string sql = @"
     INSERT INTO jaComments
     (creatorId, eventId, body, isAttending)
     VALUES
     (@creatorId, @eventId, @body, @isAttending);
     SELECT LAST_INSERT_ID();
     ";
-    int id = _db.ExecuteScalar<int>(sql, commentData);
-    commentData.Id = id;
-    return commentData;
-  }
+        int id = _db.ExecuteScalar<int>(sql, commentData);
+        commentData.Id = id;
+        return commentData;
+    }
 
-  // DELETE
-  internal void Delete(int id)
-  {
-    string sql = @"
+    // DELETE
+    internal void Delete(int id)
+    {
+        string sql = @"
     DELETE FROM jaComments WHERE id = @id; 
     ";
-    _db.Execute(sql, new { id });
-  }
+        _db.Execute(sql, new { id });
+    }
 
-  // EDIT
-  internal Boolean Edit(Comment commentData)
-  {
-    string sql = @"
+    // EDIT
+    internal Boolean Edit(Comment commentData)
+    {
+        string sql = @"
     UPDATE jaComments SET
     body = @body
     WHERE id = @id;
     ";
-    int rows = _db.Execute(sql, commentData);
-    return rows > 0;
-  }
+        int rows = _db.Execute(sql, commentData);
+        return rows > 0;
+    }
 
-  // GET ONE
-  internal Comment GetOne(int commentId)
-  {
-    string sql = @"
+    // GET ONE
+    internal Comment GetOne(int commentId)
+    {
+        string sql = @"
     SELECT
     c.*,
     a.*
@@ -56,17 +56,17 @@ public class CommentsRepository
     JOIN accounts a ON c.creatorId = a.id
     WHERE c.id = @commentId;
     ";
-    return _db.Query<Comment, Account, Comment>(sql, (c, a) =>
-    {
-      c.Creator = a;
-      return c;
-    }, new { commentId }).FirstOrDefault();
-  }
+        return _db.Query<Comment, Account, Comment>(sql, (c, a) =>
+        {
+            c.Creator = a;
+            return c;
+        }, new { commentId }).FirstOrDefault();
+    }
 
-  // GET COMMENTS BY EVENT ID
-  internal List<Comment> GetComments(int eventId)
-  {
-    string sql = @"
+    // GET COMMENTS BY EVENT ID
+    internal List<Comment> GetComments(int eventId)
+    {
+        string sql = @"
     SELECT
     c.*,
     a.*
@@ -74,29 +74,26 @@ public class CommentsRepository
     JOIN accounts a ON c.creatorId = a.id
     WHERE c.eventId = @eventId;
     ";
-    return _db.Query<Comment, Account, Comment>(sql, (c, a) =>
-    {
-      c.Creator = a;
-      return c;
-    }, new { eventId }).ToList();
-  }
+        return _db.Query<Comment, Account, Comment>(sql, (c, a) =>
+        {
+            c.Creator = a;
+            return c;
+        }, new { eventId }).ToList();
+    }
 
-  internal Comment GetByEventId(int eventId, string accountId)
-  {
-    string sql = @"
+    internal List<Comment> GetByEventId(int eventId, string accountId)
+    {
+        string sql = @"
     SELECT
     c.*,
-    e.*,
-    a.*
+    e.*
     FROM jaComments c
-    JOIN jaEvents ON e.eventId = @eventId
-    JOIN accounts on a.id = c.creatorId
-    WHERE c.eventId = @eventId
+    WHERE @eventId = e.id AND @accountId = c.creatorId;
     ";
-    return _db.Query<Comment, TowerEvent, Account, Comment>(sql, (c, e, a) =>
-    {
-      c.Creator = a;
-      return c;
-    }, new { eventId }).ToList();
-  }
+        return _db.Query<Comment, TowerEvent, Account, Comment>(sql, (c, e, a) =>
+        {
+            c.Creator = a;
+            return c;
+        }, new { eventId }).ToList();
+    }
 }
--- towerRedo/Services/TicketsService.cs
+++ towerRedo/Services/TicketsService.cs
@@ -1,101 +1,101 @@
 namespace towerRedo.Services
 {
-  public class TicketsService
-  {
-
-    private readonly TicketsRepository _repo;
-    private readonly EventsService _events;
-    private readonly CommentsRepository _commentsRepo;
-
-    public TicketsService(TicketsRepository repo, EventsService events, CommentsRepository commentsRepo)
-    {
-      _repo = repo;
-      _events = events;
-      _commentsRepo = commentsRepo;
-    }
-
-    // TODO GET TICKET BY EVENT ID
-
-    internal List<Ticket> GetByEventId(int eventId)
-    {
-      List<Ticket> tickets = _repo.GetByEventId(eventId);
-      return tickets;
-    }
-
-    // TODO GET TICKETS BY ACCOUNT ID
-
-    // CREATE
-    internal Ticket Create(Ticket ticketData)
+    public class TicketsService
     {
 
-      TowerEvent towerEvent = _events.GetOne(ticketData.EventId);
-      List<TicketEvent> accountTicket = this.GetByAccountId(ticketData.AccountId);
-      foreach (TicketEvent te in accountTicket)
-      {
-        if (te.Id == towerEvent.Id)
+        private readonly TicketsRepository _repo;
+        private readonly EventsService _events;
+        private readonly CommentsRepository _commentsRepo;
+
+        public TicketsService(TicketsRepository repo, EventsService events, CommentsRepository commentsRepo)
         {
-          throw new Exception("You already have a ticket to this event.");
+            _repo = repo;
+            _events = events;
+            _commentsRepo = commentsRepo;
         }
-      }
-      if (towerEvent.Capacity > 0)
-      {
-        towerEvent.Capacity++;
-        _events.Edit(towerEvent.Id, towerEvent);
-      }
-      else
-      {
-        throw new Exception(towerEvent.Name + " is Sold Out!");
-      }
-      Ticket ticket = _repo.Create(ticketData);
-      Comment comment = _commentsRepo.GetByEventId(ticket.EventId, ticket.AccountId);
-      return ticket;
+
+        // TODO GET TICKET BY EVENT ID
+
+        internal List<Ticket> GetByEventId(int eventId)
+        {
+            List<Ticket> tickets = _repo.GetByEventId(eventId);
+            return tickets;
+        }
+
+        // TODO GET TICKETS BY ACCOUNT ID
+
+        // CREATE
+        internal Ticket Create(Ticket ticketData)
+        {
+
+            TowerEvent towerEvent = _events.GetOne(ticketData.EventId);
+            List<TicketEvent> accountTicket = this.GetByAccountId(ticketData.AccountId);
+            foreach (TicketEvent te in accountTicket)
+            {
+                if (te.Id == towerEvent.Id)
+                {
+                    throw new Exception("You already have a ticket to this event.");
+                }
+            }
+            if (towerEvent.Capacity > 0)
+            {
+                towerEvent.Capacity--;
+                _events.Edit(towerEvent.Id, towerEvent);
+            }
+            else
+            {
+                throw new Exception(towerEvent.Name + " is Sold Out!");
+            }
+            Ticket ticket = _repo.Create(ticketData);
+            List<Comment> comments = _commentsRepo.GetByEventId(ticket.EventId, ticket.AccountId);
+            return ticket;
+        }
+
+        // DELETE
+        internal string Delete(int ticketId, string userId)
+        {
+            Ticket ticket = this.GetOne(ticketId);
+            if (ticket.AccountId != userId)
+            {
+                throw new Exception("You do not have permission to delete this ticket!");
+            }
+            Boolean isDeleted = _repo.Delete(ticket.Id);
+            if (isDeleted == false)
+            {
+                throw new Exception("Something went wrong in the repo dawg.");
+            }
+
+            TowerEvent towerEvent = _events.GetOne(ticket.EventId);
+            towerEvent.Capacity++;
+            _events.Edit(towerEvent.Id, towerEvent);
+
+            return ("Your ticket for " + towerEvent.Name + " was deleted.");
+        }
+
+        // GET BY TICKET ID
+        internal Ticket GetOne(int ticketId)
+        {
+            Ticket ticket = _repo.GetOne(ticketId);
+            if (ticket == null)
+            {
+                throw new Exception("Ticket = Null");
+            }
+            return ticket;
+        }
+
+
+        // GET BY ACCOUNT ID
+        internal List<TicketEvent> GetByAccountId(string accountId)
+        {
+            List<TicketEvent> tickets = _repo.GetByAccountId(accountId);
+            return tickets;
+        }
+
+        // GET TICKETS
+        internal List<Ticket> GetTickets(int eventId)
+        {
+            List<Ticket> tickets = _repo.GetByEventId(eventId);
+            return tickets;
+        }
     }
-
-    // DELETE
-    internal string Delete(int ticketId, string userId)
-    {
-      Ticket ticket = this.GetOne(ticketId);
-      if (ticket.AccountId != userId)
-      {
-        throw new Exception("You do not have permission to delete this ticket!");
-      }
-      Boolean isDeleted = _repo.Delete(ticket.Id);
-      if (isDeleted == false)
-      {
-        throw new Exception("Something went wrong in the repo dawg.");
-      }
-
-      TowerEvent towerEvent = _events.GetOne(ticket.EventId);
-      towerEvent.Capacity++;
-      _events.Edit(towerEvent.Id, towerEvent);
-
-      return ("Your ticket for " + towerEvent.Name + " was deleted.");
-    }
-
-    // GET BY TICKET ID
-    internal Ticket GetOne(int ticketId)
-    {
-      Ticket ticket = _repo.GetOne(ticketId);
-      if (ticket == null)
-      {
-        throw new Exception("Ticket = Null");
-      }
-      return ticket;
-    }
-
-
-    // GET BY ACCOUNT ID
-    internal List<TicketEvent> GetByAccountId(string accountId)
-    {
-      List<TicketEvent> tickets = _repo.GetByAccountId(accountId);
-      return tickets;
-    }
-
-    // GET TICKETS
-    internal List<Ticket> GetTickets(int eventId)
-    {
-      List<Ticket> tickets = _repo.GetByEventId(eventId);
-      return tickets;
-    }
-  }
 }
