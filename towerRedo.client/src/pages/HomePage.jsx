eb6a0a6b38f97e61be5282e43be012a15b7e1671
Jeanne <tebazele@gmail.com>
2023-04-06 10:37:11 -0600

    useState for filterby on homepage

--- towerRedo.client/src/pages/HomePage.jsx
+++ towerRedo.client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppState } from '../AppState.js';
 import EventCard from '../components/EventCard.jsx';
 import { eventsService } from '../services/EventsService.js';
@@ -7,6 +7,7 @@ import Pop from "../utils/Pop.js"
 import "./styles/HomePageStyle.scss"
 
 function HomePage() {
+  const [filterBy, setFilterBy] = useState('')
   async function getEvents() {
     try {
       await eventsService.getEvents()
@@ -16,13 +17,17 @@ function HomePage() {
     }
   }
 
-  let events = (AppState.events.map(e => {
-    return (
-      <div key={e.id} className="col-4 my-3">
-        <EventCard event={e} />
-      </div>
-    )
-  }))
+  function filterEvents(f) {
+    let events = AppState.events.filter(e => f ? e.type == f : true).map(e => {
+      console.log(AppState.events);
+      return (
+        <div key={e.id} className="col-4 my-3">
+          <EventCard event={e} />
+        </div>
+      )
+    })
+    return events;
+  }
 
   useEffect(() => {
     getEvents()
@@ -31,18 +36,21 @@ function HomePage() {
   return (
 
     <div className="home-page container-fluid">
+      <section className="row">
+        <h1>{filterBy}</h1>
+      </section>
       <div className='row'>
         <div style={{ backgroundImage: `url("https://images.unsplash.com/photo-1521055170349-25f955971658?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80")` }} className="bg-cover-image"></div>
         <div className='col-12 d-flex justify-content-around mt-2'>
-          <button className='btn btn-outline-dark'>All</button>
-          <button className='btn btn-outline-dark'>Concert</button>
-          <button className='btn btn-outline-dark'>Convention</button>
-          <button className='btn btn-outline-dark'>Sport</button>
-          <button className='btn btn-outline-dark'>Digital</button>
-          <button className='btn btn-outline-dark'>Other</button>
+          <button onClick={() => setFilterBy('')} className='btn btn-outline-dark'>All</button>
+          <button onClick={() => setFilterBy('Concert')} className='btn btn-outline-dark'>Concert</button>
+          <button onClick={() => setFilterBy('Convention')} className='btn btn-outline-dark'>Convention</button>
+          <button onClick={() => setFilterBy('Sport')} className='btn btn-outline-dark'>Sport</button>
+          <button onClick={() => setFilterBy('Digital')} className='btn btn-outline-dark'>Digital</button>
+          <button onClick={() => setFilterBy('Other')} className='btn btn-outline-dark'>Other</button>
 
         </div>
-        {events}
+        {filterEvents(filterBy)}
 
       </div>
     </div>
