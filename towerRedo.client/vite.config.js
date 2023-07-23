1b57f66f657c2ace1e5557c1a5141e6266ce66b9
Jeanne Allen <tebazele@gmail.com>
2023-07-23 18:04:22 +0000

    Change client build directory, add to .gitignore

--- towerRedo.client/vite.config.js
+++ towerRedo.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs',
+    outDir: 'build',
     sourcemap: false
   },
   server: {
